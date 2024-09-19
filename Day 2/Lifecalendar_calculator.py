d3c74ca86de3a14a7b1578347df317c6640e9ebb
fatmaSood <fsood53@gmail.com>
2024-09-19 15:31:16 +0300

    commit

---
+++ Day 2/Lifecalendar_calculator.py
@@ -0,0 +1,11 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+years = 90 - int(age)
+days = 365 * years
+weeks = 52 * years
+months = 12 * years
+
+print(f"You have {days} days, {weeks} weeks, and {months} months left")
