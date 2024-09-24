657c16b5c7147c7ee6c3ee9f20f5d956f7d03745
fatmaSood <fsood53@gmail.com>
2024-09-24 14:45:57 +0300

    commit

--- Day 4/RockPaperScissors.py
+++ Day 4/RockPaperScissors.py
@@ -27,32 +27,25 @@ scissors = '''
 
 # Write your code below this line 👇
 import random
+game_images = [rock, paper, scissors]
 
-user_input = input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.")
-if user_input == "0":
-    print(rock)
-elif user_input == "1":
-    print(paper)
-elif user_input == "2":
-    print(scissors)
+user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+if user_choice >= 3 or user_choice < 0:
+  print("You typed an invalid number, you lose!")
 else:
-    print("Invalid Input")
+  print(game_images[user_choice])
 
-choice = [rock, paper, scissors]
-random_choice = random.choice(choice)
-print(random_choice)
+  computer_choice = random.randint(0, 2)
+  print("Computer chose:")
+  print(game_images[computer_choice])
 
-if (user_input == scissors) and (random_choice == paper):
-    print("you lose")
-    if (user_input == paper) and (random_choice == scissors):
-        print("You win")
-elif (user_input == paper) and (random_choice == rock):
-    print("you lose")
-    if (user_input == rock) and (random_choice == paper):
-        print("You win")
-elif (user_input == rock) and (random_choice == scissors):
-    print("you lose")
-    if (user_input == scissors) and (random_choice == rock):
-        print("You win")
-else:
-    print("It is a draw")
+  if user_choice == 0 and computer_choice == 2:
+    print("You win!")
+  elif computer_choice == 0 and user_choice == 2:
+    print("You lose")
+  elif computer_choice > user_choice:
+    print("You lose")
+  elif user_choice > computer_choice:
+    print("You win!")
+  elif computer_choice == user_choice:
+    print("It's a draw")
